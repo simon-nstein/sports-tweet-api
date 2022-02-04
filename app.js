ab7d80570af91ffdbcf3cfd587e6b6d3c7922fa2
Simon Neuwirth-Stein <96710117+simon-nstein@users.noreply.github.com>
2022-02-03 21:56:34 -0800

    Add files via upload

--- app.js
+++ app.js
@@ -16,11 +16,5 @@ app.set('view engine', 'ejs')
 
 app.use(bodyParser.urlencoded({ extended : true }))
 
-// Routes
-const newsRouter = require('./src/routes/news')
-
-app.use('/', newsRouter)
-app.use('/article', newsRouter)
-
 // Listen on port 5000
 app.listen(port, () => console.log(`Listening on port ${port}`))
